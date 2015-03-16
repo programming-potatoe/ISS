e839e672aef3ffb80da2d1dd8d86f378609fa2cc
programming_potatoe <mike.ms.schmidt@gmx.de>
2015-03-16 15:22:55 +0100

    required in login_sceen.php
    tut nicht

--- Software/content/login_screen.php
+++ Software/content/login_screen.php
@@ -1,7 +1,7 @@
 <div class="loginContent">
 		<form class="login" id="login" action="./utils/login.php">
-				<input type="text" placeholder="E-Mail" id="email" /> <br>
-				<input type="password" placeholder="Passwort" id="password" /> <br>
+				<input type="text" placeholder="E-Mail" id="email" required="required"/> <br>
+				<input type="password" placeholder="Passwort" id="password" required="required"/> <br>
 				<button type="submit" class="loginButton">Login</button>
 		</form>
 </div>
