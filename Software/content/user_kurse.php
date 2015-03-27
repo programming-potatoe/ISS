2c5a622e9bce20ea4b005bd7e59c64c2a6967096
programming_potatoe <mike.ms.schmidt@gmx.de>
2015-03-27 15:36:16 +0100

    Nachtrag

--- Software/content/user_kurse.php
+++ Software/content/user_kurse.php
@@ -6,22 +6,52 @@
    require_once(__ROOT__ ."/utils/functions.php");
 
 		
+   		check_berechtigung('j', 'n', 'n', 'n', 'n');
+   		
+   		if(!isset($_GET['art'])){
+   		
+   			$_GET['art'] = 0;
+   		
+   		}
+   		//0=Startseite; 1=neuen Kurs bearbeiten; 2=Kurs updaten; 3=Kurs loeschen auswaehlen, 4=Kurs loeschen, 5= neuen Kurs anlegen, 6= Kurs insert
+   		switch ($_GET['art']){
+   		
+   			case 0:
+   				//Standardanzeige:
+   					
+   				echo 'Hier k&ouml;nnen Kurseinstellungen gemacht werden <br >Neuen Kurs <a href="content/user_kurse.php?art=5" data-change="main">hinzuf&uuml;gen</a>?<br><br>';
+   				
+   				$query = "SELECT KID, KBez FROM kurse";
+   				$result = mysql_query($query);
+   					
+   				echo "<table><tr><td>Kurs ID</td><td>KursBez</td><td></td><td></td></tr>";
+   					
+   				while ($row = mysql_fetch_assoc($result)) {
+   					echo '<tr><td>'.$row['KID'].'</td><td>'.$row['KBez'].'</td><td><a href="content/user_kurse.php?art=1&kid='.$row['KID'].'" data-change="main">bearbeiten</a></td><td><a href="content/user_kurse.php?art=3&kid='.$row['KID'].'" data-change="main">l&ouml;schen</a></td></tr>';
+   				}
+   					
+   				echo "</table>";
+   				
+   				break;
+   				
+   			case 1:
+				//neuen Kurs bearbeiten
 		
-		if(isset($_GET['id']))
-		{
 				echo("Hier wird bearbeitet! <br><br>");
 ?>
-				<form action="content/user_kurse.php">
-						Kurs ID: <br><input type="text" value="<?php echo $_GET['id']?>" name="kid" readonly/><br>
+				<form action="content/user_kurse.php?art=2">
+						Kurs ID: <br><input type="text" value="<?php echo $_GET['kid']?>" name="kid" readonly/><br>
 						Neue Kursbezeichnung: <br> <input type="text" placeholder="Kursbezeichnung" name="nkbez" /> <br>
 						<button type="submit">&Auml;ndern</button>
 				</form>
 <?php	
 				
 				echo('<a href="content/user_kurse.php" data-change="main">zur&uuml;ck</a>');			
-		}
-		else if(isset($_POST['nkbez']))
-		{
+		break;
+		
+			case 2: 
+				//Kurs updaten
+		
 				$kid = $_POST['kid'];
 				$nkbez = mysql_real_escape_string($_POST['nkbez']);
 				
@@ -35,20 +65,24 @@
 						echo "Error - Try Again<br>";
 				}
 				echo('<a href="content/user_kurse.php" data-change="main">zur&uuml;ck</a>');
-		}				
-		else if(isset($_GET['lid']))
-		{
+		break;
+
+			case 3:
+				//Kurs loeschen auswaehlen
+				
 				echo("Hier wird gel&ouml;scht! <br><br>");
 				
-				$query = "SELECT KBez FROM kurse WHERE KID = ".$_GET['lid'];				
+				$query = "SELECT KBez FROM kurse WHERE KID = ".$_GET['kid'];				
 				$row = mysql_fetch_array(mysql_query($query));
-				echo $row['KBez'].' wirklich loeschen? <a href="content/user_kurse.php?llid='.$_GET['lid'].'" data-change="main">Ja, l&ouml;schen</a><br><br>';
+				echo $row['KBez'].' wirklich loeschen? <a href="content/user_kurse.php?art=4&kid='.$_GET['kid'].'" data-change="main">Ja, l&ouml;schen</a><br><br>';
 				
 				echo('<a href="content/user_kurse.php" data-change="main">zur&uuml;ck</a>');	
-		}
-		else if(isset($_GET['llid']))
-		{
-				$query = "DELETE FROM kurse WHERE KID = ".$_GET['llid'];
+		break;
+		
+			case 4:
+				//Kurs loeschen
+				
+				$query = "DELETE FROM kurse WHERE KID = ".$_GET['kid'];
 				if(mysql_query($query)) 
 				{
 					echo "Gel&ouml;scht!<br>";
@@ -58,20 +92,22 @@
 					echo "Fehler - nicht gel&ouml;scht!<br>";
 				}
 				echo('<a href="content/user_kurse.php" data-change="main">OK, zur&uuml;ck</a>');
-		}
-		else if(isset($_GET['new']))
-		{
+		break;
+			case 5:
+				//neuen Kurs anlegen
+				
 				echo("Hier wird ein neuer Kurs hinzugef&uuml;gt! <br><br>");
 ?>
-				<form action="content/user_kurse.php">
+				<form action="content/user_kurse.php?art=6">
 						Kursbezeichnung: <br> <input type="text" placeholder="Kursbezeichnung" name="kbez" /> <br>
 						<button type="submit">Anlegen</button>
 				</form>
 <?php		
 				echo('<a href="content/user_kurse.php" data-change="main">zur&uuml;ck</a>');	
-		}
-		else if(isset($_POST['kbez']))
-		{	
+		break;
+			case 6:
+				//Kurs insert
+		
 				$kbez = mysql_real_escape_string($_POST['kbez']);
 			   
 			   $query = 'INSERT INTO kurse VALUES (NULL, "'.$kbez.'")';		   
@@ -80,29 +116,14 @@
 			   { 
 					echo "Ein neuer Kurs wurde hinzugef&uuml;gt.<br><br>";
 				
-					echo 'Einen weiteren kurs <a href="content/user_kurse.php?new=1" data-change="main">hinzuf&uuml;gen</a>?<br>';
+					echo 'Einen weiteren Kurs <a href="content/user_kurse.php?art=5" data-change="main">hinzuf&uuml;gen</a>?<br>';
 					echo '<a href="content/user_kurse.php" data-change="main">zur&uuml;ck</a>';
 				}
 				else 
 				{
 					echo 'Error - Try Again';
 				}
-		}	
-		else		
-		{	
-			//Standardanzeige:
-			
-			echo 'Hier k&ouml;nnen Kurseinstellungen gemacht werden <br >Neuen Kurs <a href="content/user_kurse.php?new=1" data-change="main">hinzuf&uuml;gen</a>?<br><br>';
-	
-			$query = "SELECT KID, KBez FROM kurse";
-			$result = mysql_query($query);
-			
-			echo "<table><tr><td>Kurs ID</td><td>KursBez</td><td></td><td></td></tr>";					
-			
-			while ($row = mysql_fetch_assoc($result)) {
-				echo '<tr><td>'.$row['KID'].'</td><td>'.$row['KBez'].'</td><td><a href="content/user_kurse.php?id='.$row['KID'].'" data-change="main">bearbeiten</a></td><td><a href="content/user_kurse.php?lid='.$row['KID'].'" data-change="main">l&ouml;schen</a></td></tr>';
-			}
-			
-			echo "</table>";
-		}
+		break;
+   		}	
+
 ?>
